3972b907b0b8c71ffee1ba3d8f23e47854fb9fd3
mypcasadu <ummek165@gmail.com>
2021-12-26 10:44:01 -0800

    CALCULATORAPP

---
+++ app/src/main/java/com/example/myareacalculatorapp/splashscreen.java
@@ -0,0 +1,34 @@
+package com.example.myareacalculatorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        Thread thread = new Thread(){
+            public void run(){
+                try {
+                    sleep(3000);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(splashscreen.this, MainActivity.class);
+                    startActivity(intent);
+
+                }
+            }
+        };thread.start();
+
+
+
+    }
+}
